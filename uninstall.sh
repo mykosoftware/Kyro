4ce5ab812d193d4fdf6be7f9d1eb7f9dec5bb4bc
Comuza-za!!! (Developer) <Comuzadeveloper@outlook.com>
2026-08-07 07:59:20 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,12 +4,7 @@ set -e
 
 echo "Desinstalando Kyro Optimizer..."
 
-if [ -f /usr/local/bin/kyro ]; then
-    sudo rm -f /usr/local/bin/kyro
-    echo "Kyro eliminado correctamente."
-else
-    echo "Kyro no está instalado."
-fi
+sudo rm -f /usr/local/bin/kyro
+sudo rm -f /usr/local/bin/kyro-uninstall
 
-echo ""
-echo "Desinstalación completada."
+echo "Kyro eliminado correctamente."
