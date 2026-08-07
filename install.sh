4c88c3508389b076d359b1ebcfc2267d03f33cfd
Comuza-za!!! (Developer) <Comuzadeveloper@outlook.com>
2026-08-06 22:05:03 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Instalando Kyro Optimizer..."
+
+sudo install -Dm755 Kyro.sh /usr/local/bin/kyro
+
+echo ""
+echo "Kyro instalado correctamente."
+echo ""
+echo "Ahora puedes ejecutar:"
+echo "  kyro"
